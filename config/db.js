cfedc5da5fbaff1f2629a99a7a7234e25ea0a6b6
mgunawardhana <mrgunawardhana27368@gmail.com>
2025-04-23 00:08:00 +0530

    content update

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // eslint-disable-next-line no-undef
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect("mongodb+srv://mrgunawardhana27368:Jb4G1jehMHXWPBLI@cluster0.cwae7cc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
             useNewUrlParser: true, useUnifiedTopology: true,
         });
         console.info('Connected to DB');
