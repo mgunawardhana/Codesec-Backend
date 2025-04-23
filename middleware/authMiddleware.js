8ee9bfc6dc211961cc84b1a33e0896f0b97f5d4b
mgunawardhana <mrgunawardhana27368@gmail.com>
2025-04-23 07:08:53 +0530

    env updated

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,11 +7,10 @@ const authMiddleware = (req, res, next) => {
         return res.status(401).json({ message: 'No token, authorization denied' });
     }
 
-    // eslint-disable-next-line no-undef
-    const jwt_sec  = process.env.JWT_SECRET;
+    // const jwt_sec  = process.env.JWT_SECRET;
 
     try {
-        req.user = jwt.verify(token, jwt_sec);
+        req.user = jwt.verify(token, "7b9f2a8d5e4c1f6b3a9d8e7c2f5b4a1d9e8c7f3a2b6d5e4c1f9b8a7d3e2c6f5");
         next();
         // eslint-disable-next-line no-unused-vars
     } catch (error) {
